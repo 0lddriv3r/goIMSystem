81b309e9e874d703c8159a71447f8421a9d3f23d
root <root@localhost.localdomain>
2022-10-06 23:33:05 +0800

    Add menu in client.

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int //当前客户端的模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       -1,
 	}
 
 	//连接server
@@ -30,6 +32,44 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	//返回对象
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1、公聊模式")
+	fmt.Println("2、私聊模式")
+	fmt.Println("3、修改用户名")
+	fmt.Println("0、退出")
+
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>> 请输入合法范围内的数字 <<<<<")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+
+		//根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			fmt.Println("公聊模式选择...")
+			break
+		case 2:
+			fmt.Println("私聊模式选择...")
+			break
+		case 3:
+			fmt.Println("修改用户名模式选择...")
+			break
+		}
+	}
 }
 
 var serverIp string
@@ -53,5 +93,5 @@ func main() {
 	fmt.Println(">>>>> 连接服务器成功 <<<<<")
 
 	//启动客户端业务
-	select {}
+	client.Run()
 }
