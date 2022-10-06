0c744545c679b765fabd3e6d409a7406a27db32c
root <root@localhost.localdomain>
2022-10-06 22:06:03 +0800

    Add User function: rename.

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name   string
@@ -63,6 +66,23 @@ func (this *User) DoMessage(msg string) {
 			this.SendMsg(onlineMsg)
 		}
 		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//修改用户名消息格式：rename|UserName
+		newName := strings.Split(msg, "|")[1]
+
+		//判断newName是否被其他人占用
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("修改用户名: " + this.Name + " 成功\n")
+		}
 	} else {
 		this.server.BroadCast(this, msg)
 	}
